4b40226ba2e032bec9bcd0a197e3f2b65039ebfc
AbdallaTarek <abdalla.taarek@gmail.com>
2024-05-27 15:53:39 +0300

    add new example implementation.

--- PayButtonExample/ViewController.swift
+++ PayButtonExample/ViewController.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2020 PaySky. All rights reserved.
 //
 
+import PayButtonIOS
 import UIKit
-import PayButton
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var transactionStatusLabel: UIButton!
     @IBOutlet weak var AppName: UILabel!
     @IBOutlet weak var MerchantIdEd: UITextField!
@@ -25,40 +24,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var RefValue: UITextField!
     @IBOutlet weak var PayBtn: UIButton!
     @IBOutlet weak var ChangeLang: UIButton!
-    
-    var receipt: TransactionStatusResponse = TransactionStatusResponse()
-    
+
+    var receipt: PayByCardReponse?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
-                                                                 action: #selector(self.dismissKeyboard))
+                                                                 action: #selector(_dismissKeyboard))
         view.addGestureRecognizer(tap)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
         AppName.text = "PaySky PayButton SDK"
-        
+
         MerchantIdLabel.text = "Merchant ID"
-        MerchantIdEd.text = "17685499712"  // set your merchant id here
-        
-        TerminalIDLabel.text =  "Terminal ID"
-        TerminalIDTF.text = "92539221" // set your terminal id here
-        
+        MerchantIdEd.text = "49660" // set your merchant id here
+
+        TerminalIDLabel.text = "Terminal ID"
+        TerminalIDTF.text = "32731520" // set your terminal id here
+
         AmountLabel.text = "Amount"
         AmountEd.text = "50.50" // set your amount here
-        
+
         RefLabel.text = "Trxn Ref Number"
-        RefValue.text = "732432535"  // set a unique number for your transaction
-        
+        RefValue.text = "732432535" // set a unique number for your transaction
+
         CurrencyLabel.text = "Currency Code"
-        CurrencyEd.text = "784"      // set country currency code, e.g. Egypt is 818
-        
+        CurrencyEd.text = "818" // set country currency code, e.g. Egypt is 818
+
         PayBtn.setTitle("Pay Now", for: .normal)
-        PayBtn.layer.cornerRadius = PaySkySDKColor.RaduisNumber
-        
+        PayBtn.layer.cornerRadius = 10
+
         ChangeLang.setTitle("Change Lang", for: .normal)
     }
 
@@ -68,78 +67,63 @@ class ViewController: UIViewController {
     }
 
     @IBAction func PayAction(_ sender: UIButton) {
-        if (MerchantIdEd.text?.isEmpty)! {
-            view.makeToast( "please enter merchant id")
-            return
-        }
-        
-        if (TerminalIDTF.text?.isEmpty)! {
-            view.makeToast( "please enter terminal id")
-            return
-        }
-        
-        if (AmountEd.text?.isEmpty)! {
-            view.makeToast( "please enter amount")
-            return
-        }
-        
-        if (CurrencyEd.text?.isEmpty)! {
-            view.makeToast( "please enter currency code")
-            return
-        }
-        
-        let paymentViewController = PaymentViewController()
-        paymentViewController.amount =  AmountEd.text!
-        paymentViewController.delegate = self
-        paymentViewController.refnumber = RefValue.text ?? ""
+        if (MerchantIdEd.text?.isEmpty)! { return }
 
-        paymentViewController.mId = MerchantIdEd.text!
-        paymentViewController.tId = TerminalIDTF.text!
-        paymentViewController.Currency = CurrencyEd.text!
-        
-        paymentViewController.isProduction = false   // set it true if you want to go live
+        if (TerminalIDTF.text?.isEmpty)! { return }
 
-        paymentViewController.Key = "31623434616334632D306463612D343036632D613563362D653239653263633439303765" // set your secure hash key here
+        if (AmountEd.text?.isEmpty)! { return }
 
+        if (CurrencyEd.text?.isEmpty)! { return }
+
+        let paymentViewController = PaymentViewController(
+            merchantId: MerchantIdEd.text!, // Mandatory
+            terminalId: TerminalIDTF.text!, // Mandatory
+            amount: Double(AmountEd.text ?? "0.0") ?? 0, // Mandatory - provide the amount and currency with it's decimal factor
+            currencyCode: Int(CurrencyEd.text!) ?? 0, // Mandatory - Provided by PaySky
+            secureHashKey: "57d29e75443b0f2ef4a1d83adfb1ab3d", // Mandatory - Provided by PaySky
+            trnxRefNumber: RefValue.text ?? "",
+            isProduction: false // Choose the needed environment
+        )
+
+        paymentViewController.delegate = self // Payment Delegate
         paymentViewController.pushViewController()
     }
-    
+
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
+            if view.frame.origin.y == 0 {
+                view.frame.origin.y -= keyboardSize.height
             }
         }
     }
-   
+
     @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
         }
     }
-    
-    @objc func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+
+    @objc func _dismissKeyboard() {
+        // Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-    
+
     @IBAction func didTapTransactionStatusLabel(_ sender: UIButton) {
-        if receipt.Success {
+        if receipt?.success == true {
             print(receipt)
         }
     }
-    
 }
 
-extension ViewController: PaymentDelegate  {
-    
-    func finishSdkPayment(_ receipt: TransactionStatusResponse) {
-        self.receipt = receipt
-        if receipt.Success {
+extension ViewController: PayButtonDelegate {
+    func finishedSdkPayment(_ response: PayButtonIOS.PayByCardReponse) {
+        self.receipt = response
+        
+        if response.success == true {
             transactionStatusLabel.setTitle("Transaction completed successfully, click here to show callback result", for: .normal)
+            navigationController?.popViewController(animated: true)
         } else {
             transactionStatusLabel.setTitle("Transaction has been failed click to callback callback ", for: .normal)
         }
     }
-    
 }
--- Podfile
+++ Podfile
@@ -1,24 +1,20 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
+install! 'cocoapods', :disable_input_output_paths => true
 
 target 'PayButtonExample' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-  
-  source 'https://github.com/payskyCompany/PayButtonIOS.git'
-  source 'https://cdn.cocoapods.org/'
-
+ 
   # Pods for PayButtonExample
-  pod 'PayButton'
-  
-  post_install do |installer|
-    installer.generated_projects.each do |project|
-      project.targets.each do |target|
-        target.build_configurations.each do |config|
-          config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
-        end
-      end
+  pod 'PayButtonIOS', :git => 'https://github.com/payskyCompany/PayButtonIOS.git', :branch => 'master'
+
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
     end
   end
-  
 end
